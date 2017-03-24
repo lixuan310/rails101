1d6947a8757d2ace092fefb2e03f72cda7f9fa1b
lixuan310 <lixuan19900214@gmail.com>
2017-03-24 22:26:12 +0800

    join & quit group action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
@@ -16,6 +15,10 @@ class User < ApplicationRecord
        participated_groups.include?(group)
      end
 
+     def join!(group)
+         participated_groups << group
+     end
+     
      def quit!(group)
        participated_groups.delete(group)
      end
